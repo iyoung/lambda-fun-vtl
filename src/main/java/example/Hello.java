31e705ce6ecc0a19099e55f6896742364a25df7b
iyoung <ian@iansramblings.com>
2017-05-07 12:24:30 +0100

    added velocity features hello world

--- src/main/java/example/Hello.java
+++ src/main/java/example/Hello.java
@@ -1,8 +1,14 @@
 package example;
 
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.Context;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeServices;
+import org.apache.velocity.runtime.RuntimeSingleton;
+
+import java.io.StringWriter;
 
 /*
 Tested with
@@ -16,7 +22,18 @@ public class Hello implements RequestHandler<Request, Response> {
 
   public Response handleRequest(Request request, Context context) {
     String greetingString = String.format("Hello %s %s.", request.firstName, request.lastName);
-    String vtl = "";
-    return new Response(greetingString + ", fetched VTL as: " + vtl);
+    String vtl = "Hello $name!  Welcome to Velocity!";
+    VelocityEngine ve = new VelocityEngine();
+    ve.init();
+    VelocityContext velocityContext = new VelocityContext();
+    velocityContext.put("name", request.firstName);
+    RuntimeServices runtimeServices = RuntimeSingleton.getRuntimeServices();
+    Template t = new Template();
+    t.setRuntimeServices(runtimeServices);
+    t.setData(vtl);
+    t.initDocument();
+    StringWriter writer = new StringWriter();
+    t.merge( velocityContext, writer );
+    return new Response(writer.toString());
   }
 }
